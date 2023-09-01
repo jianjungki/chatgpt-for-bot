899ebdb4b59147be3a143d521242b7571eca0f3a
jianjun <1188643+jianjungki@users.noreply.github.com>
2023-09-01 10:05:59 +0800

    update openai

--- config.py
+++ config.py
@@ -85,7 +85,7 @@ class OpenAIParams(BaseModel):
     min_tokens: int = 1000
     compressed_session: bool = False
     compressed_tokens: int = 1000
-    stream: bool = True
+    stream: bool = False
 
 
 class OpenAIAuths(BaseModel):
